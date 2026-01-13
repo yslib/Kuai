c0a1bcc445ca7ad0024656fb9de36221bf48a345
Shuoliu Yang <visysl@outlook.com>
2026-01-13 11:10:22 +0800

    update snapshot

--- crates/tofu-syntax/tests/parser_test.rs
+++ crates/tofu-syntax/tests/parser_test.rs
@@ -1,22 +1,22 @@
 use syntax::parser::Parser;
 use syntax::pretty::print_ast;
+use syntax::ast::Module;
+use core::Interner;
 
 #[test]
-fn test_parser_fixture() {
+fn test_parser() {
     insta::glob!("fixtures/*.tof", |path| {
-        let source_code = std::fs::read_to_string(path).unwrap();
-        let mut ctx = core::Interner::new();
-        let mut parser = Parser::new(&source_code, &mut ctx);
-        let res = parser.parse();
-        match res {
-            Ok(ast) => {
-                let pretty_output = print_ast(&ast);
-                insta::assert_snapshot!(pretty_output);
-            }
-            Err(errors) => {
-                let error_messages: Vec<String> = errors.iter().map(|e| format!("{}", e)).collect();
-                insta::assert_debug_snapshot!(error_messages);
-            }
+        let input = std::fs::read_to_string(path).unwrap();
+        let mut interner = Interner::new();
+        let mut parser = Parser::new(&input, &mut interner);
+        if let Ok(stmts) = parser.parse_impl() {
+            let ast = Module {
+                attributes: vec![],
+                stmts,
+                span: 0..input.len(),
+            };
+            let pretty_output = print_ast(&ast, &interner);
+            insta::assert_snapshot!(pretty_output);
         }
     });
 }
